443971fa72d1623bbc9471d0b22af21757000b4e
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2017-12-12 17:23:32 +0100

    WIP

--- src/dockerfile/Dockerfile/Test/StageTest.php
+++ src/dockerfile/Dockerfile/Test/StageTest.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 /*
  * This file is part of Dockerfile.
  *
- * (c) Dalibor Karlović <dkarlovi@gmail.com>
+ * (c) Dalibor Karlović <dalibor@flexolabs.io>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
